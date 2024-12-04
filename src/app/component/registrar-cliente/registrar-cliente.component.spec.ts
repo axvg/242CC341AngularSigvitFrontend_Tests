dc0a914c5c43ba170e69b1dd5a82099093da7284
axvg <axvg@users.noreply.github.com>
2024-12-03 21:48:40 -0500

    fix test

--- src/app/component/registrar-cliente/registrar-cliente.component.spec.ts
+++ src/app/component/registrar-cliente/registrar-cliente.component.spec.ts
@@ -1,4 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { ClienteService } from './../../service/cliente.service';
 
 import { RegistrarClienteComponent } from './registrar-cliente.component';
 
@@ -8,7 +10,8 @@ describe('RegistrarClienteComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [RegistrarClienteComponent]
+      imports: [HttpClientTestingModule, RegistrarClienteComponent],
+      providers: [ClienteService]
     })
     .compileComponents();
 
